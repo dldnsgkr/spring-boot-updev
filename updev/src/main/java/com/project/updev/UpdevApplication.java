c4f0949aac9b23350fba4d5b75e857e4322c1552
lee un hak <leednsgkr@naver.com>
2022-10-20 15:05:42 +0900

    first

---
+++ updev/src/main/java/com/project/updev/UpdevApplication.java
@@ -0,0 +1,13 @@
+package com.project.updev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdevApplication.class, args);
+	}
+
+}
---
+++ updev/src/test/java/com/project/updev/UpdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.updev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
